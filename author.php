fe062f53dfb3ee326d40b322e0dc974d92df84d2
Milan Svehla <milan.svehla@gmail.com>
2018-09-26 21:45:24 +0200

    1.0.0

---
+++ author.php
@@ -0,0 +1,68 @@
+<?php get_header(); ?>
+
+	<main role="main">
+		<!-- section -->
+		<section>
+
+		<?php if (have_posts()): the_post(); ?>
+
+			<h1><?php _e( 'Author Archives for ', 'theme' ); echo get_the_author(); ?></h1>
+
+		<?php if ( get_the_author_meta('description')) : ?>
+
+		<?php echo get_avatar(get_the_author_meta('user_email')); ?>
+
+			<h2><?php _e( 'About ', 'theme' ); echo get_the_author() ; ?></h2>
+
+			<?php echo wpautop( get_the_author_meta('description') ); ?>
+
+		<?php endif; ?>
+
+		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
+
+			<!-- article -->
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<!-- post thumbnail -->
+				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+					</a>
+				<?php endif; ?>
+				<!-- /post thumbnail -->
+
+				<h2>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+				</h2>
+
+				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+
+				<br class="clear">
+
+				<?php edit_post_link(); ?>
+
+			</article>
+			<!-- /article -->
+
+		<?php endwhile; ?>
+
+		<?php else: ?>
+
+			<!-- article -->
+			<article>
+
+				<h2><?php _e( 'Sorry, nothing to display.', 'theme' ); ?></h2>
+
+			</article>
+			<!-- /article -->
+
+		<?php endif; ?>
+
+			<?php get_template_part('pagination'); ?>
+
+		</section>
+	</main>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
---
+++ footer.php
@@ -0,0 +1,19 @@
+			<!-- footer -->
+			<footer class="footer" role="contentinfo">
+
+				<!-- copyright -->
+				<p class="copyright">
+					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
+				</p>
+				<!-- /copyright -->
+
+			</footer>
+			<!-- /footer -->
+
+		</div>
+		<!-- /wrapper -->
+
+		<?php wp_footer(); ?>
+
+	</body>
+</html>
