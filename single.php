6ce1aa9bae852a650425480dc090bedc53010d8c
Milan Svehla <milan.svehla@gmail.com>
2018-10-06 20:12:09 +0200

    single template cleanup

--- single.php
+++ single.php
@@ -1,35 +1,27 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
+<main role="main">
 	<section>
 
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+			<?php if ( has_post_thumbnail()) : ?>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+					<?php the_post_thumbnail(); ?>
 				</a>
 			<?php endif; ?>
-			<!-- /post thumbnail -->
 
-			<!-- post title -->
 			<h1>
 				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 			</h1>
-			<!-- /post title -->
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
+			<span class="date"><?php echo get_the_date() ?></span>
 			<span class="author"><?php _e( 'Published by', 'theme' ); ?> <?php the_author_posts_link(); ?></span>
 			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'theme' ), __( '1 Comment', 'theme' ), __( '% Comments', 'theme' )); ?></span>
-			<!-- /post details -->
 
-			<?php the_content(); // Dynamic Content ?>
+			<?php the_content(); ?>
 
 			<?php the_tags( __( 'Tags: ', 'theme' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
 
@@ -37,31 +29,20 @@
 
 			<p><?php _e( 'This post was written by ', 'theme' ); the_author(); ?></p>
 
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
 			<?php comments_template(); ?>
 
 		</article>
-		<!-- /article -->
 
 	<?php endwhile; ?>
 
 	<?php else: ?>
-
-		<!-- article -->
 		<article>
-
 			<h1><?php _e( 'Sorry, nothing to display.', 'theme' ); ?></h1>
-
 		</article>
-		<!-- /article -->
 
 	<?php endif; ?>
 
 	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+</main>
 
 <?php get_footer(); ?>
