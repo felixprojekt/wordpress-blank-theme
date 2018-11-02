7196798709adff882235ebe78956b5410db40428
Milan Svehla <milan.svehla@gmail.com>
2018-11-02 12:19:14 +0100

    page.php indents

--- page.php
+++ page.php
@@ -1,29 +1,17 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<section>
-			<h1><?php the_title(); ?></h1>
+<main role="main">
+	<section>
+		<h1><?php the_title(); ?></h1>
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
 
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
 			</article>
+		<?php endwhile; endif; ?>
 
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<article>
-				<h2><?php _e( 'Sorry, nothing to display.', 'theme' ); ?></h2>
-
-			</article>
-
-		<?php endif; ?>
-
-		</section>
-	</main>
+	</section>
+</main>
 <?php get_footer(); ?>
