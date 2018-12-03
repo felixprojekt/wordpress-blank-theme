4ad891dc3a102bf0b42f2dc4352b5fcca62552f9
Milan Svehla <milan.svehla@gmail.com>
2018-12-03 17:02:02 +0100

    footer widgets

--- footer.php
+++ footer.php
@@ -1,6 +1,17 @@
 			<!-- footer -->
 			<footer class="footer" role="contentinfo">
 
+				<?php if ( is_active_sidebar( 'widget-area-1' ) ) : ?>
+					<div id="footer-widget-1" class="footer-widget widget-area" role="complementary">
+						<?php dynamic_sidebar( 'footer-widget-1' ); ?>
+					</div>
+				<?php endif; ?>
+				<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
+					<div id="footer-widget-2" class="footer-widget widget-area" role="complementary">
+						<?php dynamic_sidebar( 'footer-widget-2' ); ?>
+					</div>
+				<?php endif; ?>
+
 				<!-- copyright -->
 				<p class="copyright">
 					&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>.
