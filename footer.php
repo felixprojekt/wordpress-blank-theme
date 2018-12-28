05982395f667827da4fa311f0d7e3b7db25b9da2
Milan Svehla <milan.svehla@gmail.com>
2018-12-28 23:05:31 +0100

    correct footer widgets

--- footer.php
+++ footer.php
@@ -2,13 +2,13 @@
 			<footer class="footer" role="contentinfo">
 
 				<?php if ( is_active_sidebar( 'widget-area-1' ) ) : ?>
-					<div id="footer-widget-1" class="footer-widget widget-area" role="complementary">
-						<?php dynamic_sidebar( 'footer-widget-1' ); ?>
+					<div id="widget-area-1" class="footer-widget widget-area" role="complementary">
+						<?php dynamic_sidebar( 'widget-area-1' ); ?>
 					</div>
 				<?php endif; ?>
 				<?php if ( is_active_sidebar( 'widget-area-2' ) ) : ?>
-					<div id="footer-widget-2" class="footer-widget widget-area" role="complementary">
-						<?php dynamic_sidebar( 'footer-widget-2' ); ?>
+					<div id="widget-area-2" class="footer-widget widget-area" role="complementary">
+						<?php dynamic_sidebar( 'widget-area-2' ); ?>
 					</div>
 				<?php endif; ?>
 
