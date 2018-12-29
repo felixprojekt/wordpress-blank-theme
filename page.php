cf3a89be6541d9c7be29c9bc026e888cfe65e9ef
Milan Svehla <milan.svehla@gmail.com>
2018-12-29 01:44:18 +0100

    page template change

--- page.php
+++ page.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
 
 <main role="main">
-	<section>
+	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
 		<h1><?php the_title(); ?></h1>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				<?php the_content(); ?>
-
-			</article>
-		<?php endwhile; endif; ?>
-
-	</section>
+		<section>
+			<?php the_content(); ?>
+		</section>
+		
+	<?php endwhile; endif; ?>
 </main>
 <?php get_footer(); ?>
