0bbecc4ee351dbd8fc4c6647eefc4e589bda03ef
Milan Svehla <milan.svehla@gmail.com>
2019-07-17 14:30:25 +0200

    comment cleanup

--- author.php
+++ author.php
@@ -1,9 +1,7 @@
 <?php get_header(); ?>
 
 	<main role="main">
-		<!-- section -->
 		<section>
-
 		<?php if (have_posts()): the_post(); ?>
 
 			<h1><?php _e( 'Author Archives for ', 'theme' ); echo get_the_author(); ?></h1>
@@ -19,50 +17,37 @@
 		<?php endif; ?>
 
 		<?php rewind_posts(); while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<!-- post thumbnail -->
-				<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+				<?php if ( has_post_thumbnail()) : ?>
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-						<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+						<?php the_post_thumbnail(array(120,120)); ?>
 					</a>
 				<?php endif; ?>
-				<!-- /post thumbnail -->
 
 				<h2>
 					<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
 				</h2>
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<?php html5wp_excerpt('html5wp_index'); ?>
 
 				<br class="clear">
 
 				<?php edit_post_link(); ?>
 
 			</article>
-			<!-- /article -->
 
 		<?php endwhile; ?>
-
 		<?php else: ?>
-
-			<!-- article -->
 			<article>
 
 				<h2><?php _e( 'Sorry, nothing to display.', 'theme' ); ?></h2>
 
 			</article>
-			<!-- /article -->
-
 		<?php endif; ?>
-
 			<?php get_template_part('pagination'); ?>
-
 		</section>
 	</main>
-
 <?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- loop.php
+++ loop.php
@@ -2,9 +2,9 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-		<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
+		<?php if ( has_post_thumbnail()) : ?>
 			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-				<?php the_post_thumbnail(array(120,120)); // Declare pixel size you need inside the array ?>
+				<?php the_post_thumbnail(array(120,120)); ?>
 			</a>
 		<?php endif; ?>
 
@@ -15,18 +15,14 @@
 		<span class="date"><?php echo get_the_date(); ?></span>
 		<span class="author"><?php _e( 'Published by', 'theme' ); ?> <?php the_author_posts_link(); ?></span>
 
-		<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+		<?php html5wp_excerpt('html5wp_index'); ?>
 
 	</article>
 
 <?php endwhile; ?>
 
 <?php else: ?>
-
-	<!-- article -->
 	<article>
 		<h2><?php _e( 'Sorry, nothing to display.', 'theme' ); ?></h2>
 	</article>
-	<!-- /article -->
-
 <?php endif; ?>
