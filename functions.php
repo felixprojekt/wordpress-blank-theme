b5cac466a148caa20d962bcab849dc07194e3a60
felixprojekt <milan.svehla@gmail.com>
2019-09-25 19:57:29 +0200

    remove separate responsive scss file

--- functions.php
+++ functions.php
@@ -1,12 +1,11 @@
 <?php
 /*------------------------------------*\
-	Theme Support
+    Theme Support
 \*------------------------------------*/
 
-define("THEME_VERSION", "1"); //use to refresh CSS and JS cache if needed
+define('THEME_VERSION', '1'); //use to refresh CSS and JS cache if needed
 
-if (function_exists('add_theme_support'))
-{
+if (function_exists('add_theme_support')) {
     // Add Menu Support
     add_theme_support('menus');
 
@@ -21,50 +20,50 @@ if (function_exists('add_theme_support'))
     add_theme_support('automatic-feed-links');
 
     // Localisation Support
-    load_theme_textdomain('theme', get_template_directory() . '/languages');
+    load_theme_textdomain('theme', get_template_directory().'/languages');
 }
 
 /*------------------------------------*\
-	Functions
+    Functions
 \*------------------------------------*/
 
 // navigation
 function theme_nav()
 {
-	wp_nav_menu(
-	array(
-		'theme_location'  => 'header-menu',
-		'menu'            => '',
-		'container'       => 'div',
-		'container_class' => 'menu-{menu slug}-container',
-		'container_id'    => '',
-		'menu_class'      => 'menu',
-		'menu_id'         => '',
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'before'          => '',
-		'after'           => '',
-		'link_before'     => '',
-		'link_after'      => '',
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 0,
-		'walker'          => ''
-		)
-	);
+    wp_nav_menu(
+        [
+            'theme_location' => 'header-menu',
+            'menu' => '',
+            'container' => 'div',
+            'container_class' => 'menu-{menu slug}-container',
+            'container_id' => '',
+            'menu_class' => 'menu',
+            'menu_id' => '',
+            'echo' => true,
+            'fallback_cb' => 'wp_page_menu',
+            'before' => '',
+            'after' => '',
+            'link_before' => '',
+            'link_after' => '',
+            'items_wrap' => '<ul>%3$s</ul>',
+            'depth' => 0,
+            'walker' => '',
+        ]
+    );
 }
 
 function theme_styles()
 {
-    wp_enqueue_style('theme', get_template_directory_uri() . '/style.css', array(), THEME_VERSION, 'all');
-    wp_enqueue_style('main', get_template_directory_uri() . '/css/main.css', array(), THEME_VERSION, 'all');
+    wp_enqueue_style('theme', get_template_directory_uri().'/style.css', [], THEME_VERSION, 'all');
+    wp_enqueue_style('main', get_template_directory_uri().'/css/main.css', [], THEME_VERSION, 'all');
 
-    if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
-        wp_enqueue_script('themescripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), THEME_VERSION, true); 
+    if ('wp-login.php' != $GLOBALS['pagenow'] && !is_admin()) {
+        wp_enqueue_script('themescripts', get_template_directory_uri().'/js/scripts.js', ['jquery'], THEME_VERSION, true);
     }
 
     // Example of conditional script
     // if (is_page('pagenamehere')) {
-    //     wp_register_script('scriptname', get_template_directory_uri() . '/js/scriptname.js', array('jquery'), THEME_VERSION, true); 
+    //     wp_register_script('scriptname', get_template_directory_uri() . '/js/scriptname.js', array('jquery'), THEME_VERSION, true);
     //     wp_enqueue_script('scriptname');
     // }
 }
@@ -72,22 +71,23 @@ function theme_styles()
 // Register navigation
 function register_html5_menu()
 {
-    register_nav_menus(array( // Using array to specify more menus if needed
+    register_nav_menus([ // Using array to specify more menus if needed
         'header-menu' => __('Header Menu', 'theme'), // Main Navigation
-    ));
+    ]);
 }
 
 // Remove the <div> surrounding the dynamic navigation to cleanup markup
 function my_wp_nav_menu_args($args = '')
 {
     $args['container'] = false;
+
     return $args;
 }
 
 // Remove Injected classes, ID's and Page ID's from Navigation <li> items
 function my_css_attributes_filter($var)
 {
-    return is_array($var) ? array() : '';
+    return is_array($var) ? [] : '';
 }
 
 // Remove invalid rel attribute values in the categorylist
@@ -115,27 +115,26 @@ function add_slug_to_body_class($classes)
 }
 
 // If Dynamic Sidebar Exists
-if (function_exists('register_sidebar'))
-{
-    register_sidebar(array(
+if (function_exists('register_sidebar')) {
+    register_sidebar([
         'name' => __('Widget Area 1', 'theme'),
         'description' => __('Description for this widget-area...', 'theme'),
         'id' => 'widget-area-1',
         'before_widget' => '<div id="%1$s" class="%2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
+        'after_title' => '</h3>',
+    ]);
 
-    register_sidebar(array(
+    register_sidebar([
         'name' => __('Widget Area 2', 'theme'),
         'description' => __('Description for this widget-area...', 'theme'),
         'id' => 'widget-area-2',
         'before_widget' => '<div id="%1$s" class="%2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
+        'after_title' => '</h3>',
+    ]);
 }
 
 // Pagination for paged posts, Page 1, Page 2, Page 3, with Next and Previous Links, No plugin
@@ -143,19 +142,20 @@ function html5wp_pagination()
 {
     global $wp_query;
     $big = 999999999;
-    echo paginate_links(array(
+    echo paginate_links([
         'base' => str_replace($big, '%#%', get_pagenum_link($big)),
         'format' => '?paged=%#%',
         'current' => max(1, get_query_var('paged')),
-        'total' => $wp_query->max_num_pages
-    ));
+        'total' => $wp_query->max_num_pages,
+    ]);
 }
 
 // Custom View Article link to Post
 function html5_blank_view_article($more)
 {
     global $post;
-    return '... <a class="view-article" href="' . get_permalink($post->ID) . '">' . __('View Article', 'theme') . '</a>';
+
+    return '... <a class="view-article" href="'.get_permalink($post->ID).'">'.__('View Article', 'theme').'</a>';
 }
 
 // Remove Admin bar
@@ -171,24 +171,23 @@ function html5_style_remove($tag)
 }
 
 // Remove thumbnail width and height dimensions that prevent fluid images in the_thumbnail
-function remove_thumbnail_dimensions( $html )
+function remove_thumbnail_dimensions($html)
 {
-    $html = preg_replace('/(width|height)=\"\d*\"\s/', "", $html);
-    return $html;
+    return preg_replace('/(width|height)=\"\d*\"\s/', '', $html);
 }
 
 // Threaded Comments
 function enable_threaded_comments()
 {
     if (!is_admin()) {
-        if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+        if (is_singular() and comments_open() and (1 == get_option('thread_comments'))) {
             wp_enqueue_script('comment-reply');
         }
     }
 }
 
 /*------------------------------------*\
-	Actions + Filters + ShortCodes
+    Actions + Filters + ShortCodes
 \*------------------------------------*/
 // Add Actions
 add_action('get_header', 'enable_threaded_comments'); // Enable Threaded Comments
@@ -232,58 +231,59 @@ add_filter('image_send_to_editor', 'remove_thumbnail_dimensions', 10); // Remove
 remove_filter('the_excerpt', 'wpautop'); // Remove <p> tags from Excerpt altogether
 
 /*------------------------------------*\
-	Custom Post Types
+    Custom Post Types
 \*------------------------------------*/
 
 function create_post_type_html5()
 {
     register_post_type('html5-blank', // Register Custom Post Type
-        array(
-        'labels' => array(
-            'name' => __('CustomPosts', 'theme'),
-            'singular_name' => __('CustomPost', 'theme'),
-            'add_new' => __('Add New', 'theme'),
-            'add_new_item' => __('Add New Post', 'theme'),
-            'edit' => __('Edit', 'theme'),
-            'edit_item' => __('Edit Post', 'theme'),
-            'new_item' => __('New Post', 'theme'),
-            'view' => __('View', 'theme'),
-            'view_item' => __('View Post', 'theme'),
-            'view_items' => __('View Posts', 'theme'),
-            'search_items' => __('Search Posts', 'theme'),
-            'not_found' => __('No posts found', 'theme'),
-            'not_found_in_trash' => __('No posts found in Trash', 'theme')
-        ),
-        'public' => true,
-        'can_export' => true, // Allows export in Tools > Export
-        'show_in_rest' => false, // Set this to true if you want to use Gutenberg with this CPT
-        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-        'has_archive' => true,
-        'menu_icon' => 'dashicons-welcome-write-blog',
-        'supports' => array(
-            'title',
-            'editor',
-            'thumbnail',
-            'revisions',
-            // 'page-attributes', // Allows menu_order and other page-like attributes
-        ), 
-    ));
+        [
+            'labels' => [
+                'name' => __('CustomPosts', 'theme'),
+                'singular_name' => __('CustomPost', 'theme'),
+                'add_new' => __('Add New', 'theme'),
+                'add_new_item' => __('Add New Post', 'theme'),
+                'edit' => __('Edit', 'theme'),
+                'edit_item' => __('Edit Post', 'theme'),
+                'new_item' => __('New Post', 'theme'),
+                'view' => __('View', 'theme'),
+                'view_item' => __('View Post', 'theme'),
+                'view_items' => __('View Posts', 'theme'),
+                'search_items' => __('Search Posts', 'theme'),
+                'not_found' => __('No posts found', 'theme'),
+                'not_found_in_trash' => __('No posts found in Trash', 'theme'),
+            ],
+            'public' => true,
+            'can_export' => true, // Allows export in Tools > Export
+            'show_in_rest' => false, // Set this to true if you want to use Gutenberg with this CPT
+            'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
+            'has_archive' => true,
+            'menu_icon' => 'dashicons-welcome-write-blog',
+            'supports' => [
+                'title',
+                'editor',
+                'thumbnail',
+                'revisions',
+                // 'page-attributes', // Allows menu_order and other page-like attributes
+            ],
+        ]);
 }
 
 //Edit post link to open in new tab
-add_filter( 'edit_post_link', function( $link, $post_id, $text )
-{
-    // Add the target attribute 
-    if( false === strpos( $link, 'target=' ) )
-        $link = str_replace( '<a ', '<a target="_blank" ', $link );
+add_filter('edit_post_link', function ($link, $post_id, $text) {
+    // Add the target attribute
+    if (false === strpos($link, 'target=')) {
+        $link = str_replace('<a ', '<a target="_blank" ', $link);
+    }
 
     return $link;
-}, 10, 3 );
+}, 10, 3);
 
 add_action('admin_head', 'wbt_admin_css');
 
-function wbt_admin_css() {
-  echo '<style>
+function wbt_admin_css()
+{
+    echo '<style>
   .wp-block {
         max-width: 1300px;
     }
@@ -291,18 +291,21 @@ function wbt_admin_css() {
 }
 
 /**
-* Create Logo Setting and Upload Control
-*/
-function brand_logo_in_customizer($wp_customize) {
+ * Create Logo Setting and Upload Control.
+ *
+ * @param mixed $wp_customize
+ */
+function brand_logo_in_customizer($wp_customize)
+{
     $wp_customize->add_setting('header_logo');
-    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'header_logo',
-    array(
-        'label' => 'Nahrát logo',
-        'section' => 'title_tagline',
-        'settings' => 'header_logo',
-    ) ) );
-
+    $wp_customize->add_control(new WP_Customize_Image_Control(
+        $wp_customize,
+        'header_logo',
+        [
+            'label' => 'Nahrát logo',
+            'section' => 'title_tagline',
+            'settings' => 'header_logo',
+        ]
+    ));
 }
 add_action('customize_register', 'brand_logo_in_customizer');
-
-?>
