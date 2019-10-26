a9f3626c9f415be45ee674aa3bf13e9b06b7977e
felixprojekt <milan.svehla@gmail.com>
2019-10-26 19:06:56 +0200

    wrap page.php content in section

--- page.php
+++ page.php
@@ -1,14 +1,16 @@
 <?php get_header(); ?>
 
 <main role="main">
+	<section>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
 		<h1><?php the_title(); ?></h1>
 
-		<section>
+		
 			<?php the_content(); ?>
-		</section>
+		
 		
 	<?php endwhile; endif; ?>
+	</section>
 </main>
 <?php get_footer(); ?>
